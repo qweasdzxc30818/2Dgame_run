ff06760b46ded36520e3f9fe2c11c63950e34470
qweasdzxc30818 <54761435+qweasdzxc30818@users.noreply.github.com>
2019-11-03 15:09:28 +0800

    血條設定(可參考)

--- 2D_run/Assets/scripts/dog.cs
+++ 2D_run/Assets/scripts/dog.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class dog : MonoBehaviour
 {
@@ -9,18 +10,28 @@ public class dog : MonoBehaviour
     [Range(1,10)]
     public int jumpCount = 2;   //field欄位(變數)
     [Header("跳躍高度")]//(可加在同個中括號 但要逗號)
-    [Range(0,600)]
+    [Range(0,1500)]
     public int jump;
     [Range(0, 10.5f)]
     public float moveSpeed = 1.5f;
     [Tooltip("判定是否在地上")]//tooltip(文字描述)
     public bool isGround; //布林 true or false
     public string Name = "Tuna";
-    public Transform cha, cam;//可以儲存物件的控制
-    public Animator ani;
-    public CapsuleCollider2D cc2d;
-    public Rigidbody2D Rgi2D;
+    public AudioClip Jump_1, Slide_1;//音源片段
+    private Transform  cam;//可以儲存物件的控制
+    private Animator ani;//儲存動畫器
+    private CapsuleCollider2D cc2d;//碰撞器
+    private Rigidbody2D Rgi2D;//剛體
+    private AudioSource audio;//音源
+    private SpriteRenderer sr;//物體顯隱
+    [Header("血量")]
+    public float health;
+    private float maxHp;
+    public Image hp;
+    public float dam;//障礙物傷害
+
     #endregion
+
     #region 事件
     //開始事件 遊戲開始時執行一次
     private void Start()
@@ -28,16 +39,18 @@ public class dog : MonoBehaviour
         cam = GameObject.Find("Main Camera").transform;
         ani = GetComponent<Animator>();
         cc2d = GetComponent<CapsuleCollider2D>();
+        audio = GetComponent<AudioSource>();
         Rgi2D = GetComponent<Rigidbody2D>();
-        print("hello");
+        sr = GetComponent<SpriteRenderer>();
+        maxHp = health;
     }
 
     //更新事件 每一禎執行一次 約60fps
     private void Update()
     {
-        print("hey");
         movecha();
         movecam();
+        
     }
     /// <summary>
     /// 判定是否在地上
@@ -50,8 +63,36 @@ public class dog : MonoBehaviour
             isGround = true;
         }
     }
+
+    /// <summary>
+    /// 觸碰障礙物時執行
+    /// </summary>
+    /// <param name="collision"></param>
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "障礙物")
+        {
+            health = health - dam;
+            Damage();
+            hp.fillAmount = health / maxHp;
+            sr.enabled = false;
+            Invoke("Show", .3f);
+        }
+    }
     #endregion
+
     #region 方法
+    private void Show()
+    {
+        sr.enabled = true;
+    }
+    /// <summary>
+    /// 角色受傷
+    /// </summary>
+    private void Damage()
+    {
+        Debug.Log("Hurt!");
+    }
     /// <summary>
     ///角色移動方法 
     /// </summary>
@@ -78,6 +119,7 @@ public class dog : MonoBehaviour
             print("跳躍");
             ani.SetBool("跳躍開關", true);
             Rgi2D.AddForce(new Vector2(0,jump));
+            audio.PlayOneShot(Jump_1, 0.7f);
             isGround = false;
         }
     }
@@ -91,6 +133,7 @@ public class dog : MonoBehaviour
         ani.SetBool("滑行開關", true);
         cc2d.offset = new Vector2(0.02f, -0.78f);
         cc2d.size = new Vector2(0.97f, 1.09f);
+        audio.PlayOneShot(Slide_1, 0.7f);
     }
 
     /// <summary>
